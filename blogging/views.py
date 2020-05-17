42e8fb9e921bfa56790bc5e72aa864822b513e02
joejohnsto <joejohnsto@gmail.com>
2020-05-16 17:08:28 -0700

    detail view added with link from front end

--- blogging/views.py
+++ blogging/views.py
@@ -23,3 +23,13 @@ def list_view(request):
     context = {'posts': posts}
     # body = template.render(context)
     return render(request, 'blogging/list.html', context)#HttpResponse(body, content_type="text/html")
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
